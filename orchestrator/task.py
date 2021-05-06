41361017f0249798163cac9fc5f883b8948e14d8
Justin Toribio <toribio.justin@gmail.com>
2021-05-06 16:23:14 -0700

    TrainModel task completed and tested

--- orchestrator/task.py
+++ orchestrator/task.py
@@ -56,7 +56,8 @@ class DownloadData(DockerTask):
 
 
 class MakeDatasets(DockerTask):
-
+    """Pipeline task to make train and test datasets ready for ML model."""
+    
     out_dir = luigi.Parameter(default='/usr/share/data/datasets/')
 
     @property
@@ -68,8 +69,6 @@ class MakeDatasets(DockerTask):
 
     @property
     def command(self):
-        # TODO: implement correct command
-        # Try to get the input path from self.requires() ;)
         return [
             'python', 'dataset.py',
             '--in-csv', self.input().path,
@@ -79,4 +78,35 @@ class MakeDatasets(DockerTask):
     def output(self):
         return luigi.LocalTarget(
             path=str(Path(self.out_dir) / '.SUCCESS')
+        )
+
+
+class TrainModel(DockerTask):
+    """Pipeline task to train a ML model."""
+
+    fname = luigi.Parameter(default='model')
+    out_dir = luigi.Parameter(default='/usr/share/data/model/')
+
+    @property
+    def image(self):
+        return f'code-challenge/train-model:{VERSION}'
+
+    def requires(self):
+        return MakeDatasets()
+
+    @property
+    def command(self):
+        return [
+            'python', 'train_model.py',
+            '--name', self.fname,
+            '--x-train', self.input().path.replace('.SUCCESS', 'X_train.pkl'),
+            '--y-train', self.input().path.replace('.SUCCESS', 'y_train.pkl'),
+            '--x-test', self.input().path.replace('.SUCCESS', 'X_test.pkl'),
+            '--y-test', self.input().path.replace('.SUCCESS', 'y_test.pkl'),
+            '--out-dir', self.out_dir
+        ]
+
+    def output(self):
+        return luigi.LocalTarget(
+            path=str(Path(self.out_dir) / f'{self.fname}.json')
         )
